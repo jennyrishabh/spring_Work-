231022f401a9a5bb552031ed151f73b786adc58e
Rishabh Jain <69865046+jennyrishabh@users.noreply.github.com>
2021-03-04 23:48:45 +0530

    Add files via upload

---
+++ spring_maven/first/src/main/java/AOP_first/first/config.java
@@ -0,0 +1,12 @@
+package AOP_first.first;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan("AOP_first.first")
+@EnableAspectJAutoProxy
+public class config {
+
+}
